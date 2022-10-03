ecc33d329bcff967303a88c5543ec285931ecada
michaelatma21 <mekyos.sfw@gmail.com>
2022-10-03 19:23:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10786/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10786
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur",
+        "Kebiasaan sehari-hari"),
+        Task("Chapter 2", "Ngek", "Kegiatan ngek"),
+        Task("Chapter 3", "Ngok", "Kegiatan ngok"),
+        Task("Chapter 4", "Ngueng", "Kegiatan ngueng"),
+        Task("Chapter 5", "Tamat", "Kegiatan tamat saatnya absen adik adik")
+    )
+}
